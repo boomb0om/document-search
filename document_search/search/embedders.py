27d2be647cdb3b8f874d27e6b917dbf53a341b89
stasstaf <stasstaf2002@gmail.com>
2024-09-08 13:17:30 +0300

    codestyle fixes

--- document_search/search/embedders.py
+++ document_search/search/embedders.py
@@ -1,13 +1,16 @@
-from document_search.entities import TextDocEntity, VectorizedDocEntity
-from document_search.search.interfaces import TextEntityEmbedder
 import numpy as np
 from langchain_community.embeddings import HuggingFaceEmbeddings  # type: ignore
+
+from document_search.entities import TextDocEntity, VectorizedDocEntity
+from document_search.search.interfaces import TextEntityEmbedder
 
 
 class TextEntityEmbedderE5(TextEntityEmbedder):
 
     def __init__(self) -> None:
-        self.embedder = HuggingFaceEmbeddings(model_name='intfloat/multilingual-e5-base')
+        self.embedder = HuggingFaceEmbeddings(
+            model_name='intfloat/multilingual-e5-base'
+        )
 
     def vectorize(self, item: TextDocEntity) -> VectorizedDocEntity:
         vector = np.array(self.embedder.embed_documents([item.text]))
