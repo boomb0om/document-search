af46d41b69db9f0d154593f5fd9bae57477e8985
stasstaf <stasstaf2002@gmail.com>
2024-09-08 13:33:01 +0300

    pytest instead of unittest

--- tests/test_embedders.py
+++ tests/test_embedders.py
@@ -1,26 +1,17 @@
-import unittest
 import numpy as np
 from document_search.entities import EntityPosition, TextDocEntity
 from document_search.search.embedders import TextEntityEmbedderE5
 
+def test_vectorize():
+    position = EntityPosition(parent_document_id="doc1", page=1, position_x=0.5, position_y=0.5)
+    text_entity = TextDocEntity(id_=1, position=position, text="This is a sample text")
 
-class TestTextEntityEmbedderE5(unittest.TestCase):
+    embedder = TextEntityEmbedderE5()
 
-    def test_vectorize(self):
-        position = EntityPosition(parent_document_id="doc1", page=1, position_x=0.5, position_y=0.5)
-        text_entity = TextDocEntity(id_=1, position=position, text="This is a sample text")
+    vectorized_entity = embedder.vectorize(text_entity)
 
-        embedder = TextEntityEmbedderE5()
+    assert vectorized_entity.id_ == text_entity.id_
+    assert vectorized_entity.position == text_entity.position
 
-        vectorized_entity = embedder.vectorize(text_entity)
-
-        self.assertEqual(vectorized_entity.id_, text_entity.id_)
-        self.assertEqual(vectorized_entity.position, text_entity.position)
-
-        self.assertIsInstance(vectorized_entity.vector, np.ndarray)
-        self.assertEqual(vectorized_entity.vector.shape,
-                         (1, 768))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert isinstance(vectorized_entity.vector, np.ndarray)
+    assert vectorized_entity.vector.shape == (1, 768)
