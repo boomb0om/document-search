944d0437254a3f85d859a79582abe0be71f973d5
UsefulTornado <Azarel.url@gmail.com>
2024-09-12 18:39:33 +0300

    chore: change name

--- frontend/api.py
+++ frontend/api.py
@@ -4,29 +4,29 @@ from typing import Any
 
 import requests
 
-API_URL = os.environ.get("API_URL")
+BACKEND_API_URL = os.environ.get("BACKEND_API_URL")
 
 
 def add_document(file: io.BytesIO) -> Any:
-    response = requests.put(f"{API_URL}/documents/add/", files={"file": file})
+    response = requests.put(f"{BACKEND_API_URL}/documents/add/", files={"file": file})
     return response.json()
 
 
 def get_uploading_status(doc_id: str) -> Any:
     response = requests.get(
-        f"{API_URL}/documents/status/", params={"documentID": doc_id}
+        f"{BACKEND_API_URL}/documents/status/", params={"documentID": doc_id}
     )
     return response.json()
 
 
 def get_storage_info() -> Any:
-    response = requests.get(f"{API_URL}/documents/storage_info/")
+    response = requests.get(f"{BACKEND_API_URL}/documents/storage_info/")
     return response.json()
 
 
 def search_query(query: str, document_ids: list[str] | None, use_rag: bool) -> Any:
     response = requests.post(
-        f"{API_URL}/search/query/",
+        f"{BACKEND_API_URL}/search/query/",
         json={"query": query, "document_ids": document_ids, "use_rag": use_rag},
     )
     return response.json()
@@ -34,7 +34,7 @@ def search_query(query: str, document_ids: list[str] | None, use_rag: bool) -> A
 
 def get_image_by_id(document_id: str, page: int) -> io.BytesIO:
     response = requests.get(
-        f"{API_URL}/documents/get_image/",
+        f"{BACKEND_API_URL}/documents/get_image/",
         json={"document_id": document_id, "page": page},
     )
     return io.BytesIO(response.content)
